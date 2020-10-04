60a443473a1ffb5d63d9fd36a060f841eab70f27
Nithin Joy <56895565+nithin-j@users.noreply.github.com>
2020-10-04 01:39:47 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,4 @@
+let itemCategory = document.querySelector('#itemCategory');
+let inputItem = document.querySelector('#inputItem');
+let btnAddItem = document.getElementById('btn-add-item');
+btnAddItem.addEventListener('click', addNewItem);
